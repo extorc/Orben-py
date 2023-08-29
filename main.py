55553ec36e090b4ebb817b86cd397808a1a91e71
Extorc <mittalshaurya004@gmail.com>
2023-08-29 15:38:16 +0530

    Moom gravity

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ pixelScale = 300
 body = Body([0, 200, 0], Vec(700, 0), 6380/pixelScale)
 body2 = NBody([200, 0, 0], Vec(700 + 6500/pixelScale, 0), 2)
 body3 = Body([200, 200, 200], Vec(-580, 0), 1736/pixelScale)
-body2.velocity.y = 10.916/300
+body2.velocity.y = 10.976/300
+#10.976 escapes the moon orbit
 
 active = True
 cycle = 0
@@ -33,11 +34,15 @@ while active:
    pointList.append(Vec.ToTuple(Body.PositionInPlane(screen, body2.position)))
 
    distSQ = pow(body.position.x - body2.position.x, 2) + pow(body.position.y - body2.position.y, 2)
+   distSQ2 = pow(body3.position.x - body2.position.x, 2) + pow(body3.position.y - body2.position.y, 2)
    auth_g = CalculateG(6.6743 * 10**-11, 5.9721 * 10**24, distSQ)
+   auth_g2 = CalculateG(6.6743 * 10**-11, 5.9721 * 10**24, distSQ2)
 
    slope = math.atan2(body2.position.y - body.position.y,body2.position.x - body.position.x)
+   slope2 = math.atan2(body2.position.y - body3.position.y,body2.position.x - body3.position.x)
    g = Vec((auth_g/(pixelScale * 1000)) * math.cos(slope), (auth_g/(pixelScale * 1000)) * math.sin(slope))
-   body2.velocity = body2.velocity - g
+   g2 = Vec((auth_g2/(pixelScale * 1000)) * math.cos(slope2), (auth_g2/(pixelScale * 1000)) * math.sin(slope2))
+   body2.velocity = body2.velocity - (g+g2)
 
    pygame.draw.lines(screen, (255, 0, 0), False, pointList)
    pygame.draw.line(screen, (200, 0, 0), Vec.ToTuple(Body.PositionInPlane(screen, body2.position)),Vec.ToTuple(Body.PositionInPlane(screen, body.position)))
@@ -45,7 +50,7 @@ while active:
    text = my_font.render(f'{math.sqrt(distSQ) * pixelScale} Km', False, (255, 0, 0))
    text2 = my_font.render(f'{body2.velocity.Mag() * pixelScale} km/s', False, (255, 0, 0))
    text3 = my_font.render(f'{g.Mag() * (pixelScale * 1000)} m/s2', False, (255, 0, 0))
-   text4 = my_font.render(f'{cycle} s', False, (255, 0, 0))
+   text4 = my_font.render(f'{cycle//3600} h', False, (255, 0, 0))
 
    screen.blit(text, (0,0))
    screen.blit(text2, (0,25))
