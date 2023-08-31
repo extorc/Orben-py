5d3743777b457c862c4e73a2d7b08113f44dbf91
Extorc <mittalshaurya004@gmail.com>
2023-08-31 20:01:37 +0530

    cleanup

--- Phy.py
+++ Phy.py
@@ -1,2 +1,12 @@
+import math
+from Vec import Vec
+pixelScale = 300
+
 def CalculateG(G, mass, distanceSQ):
     return (G * mass) / (9 * 10 ** 10 * distanceSQ)
+
+def PhyUpdate(body, body2, body_mass):
+   distSQ = pow(body.position.x - body2.position.x, 2) + pow(body.position.y - body2.position.y, 2)
+   auth_g = CalculateG(6.6743 * 10**-11, body_mass, distSQ)
+   slope = math.atan2(body2.position.y - body.position.y,body2.position.x - body.position.x)
+   return Vec((auth_g/(pixelScale * 1000)) * math.cos(slope), (auth_g/(pixelScale * 1000)) * math.sin(slope)), distSQ
--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import pygame
 from Body import Body, NBody
 import math
 from Vec import Vec
-from Phy import CalculateG
+from Phy import CalculateG, pixelScale, PhyUpdate
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.font.init()
 my_font = pygame.font.SysFont('Mono', 20)
 clock = pygame.time.Clock()
 
-pixelScale = 300
 body = Body([0, 200, 0], Vec(700, 0), 6380/pixelScale)
 body2 = NBody([200, 0, 0], Vec(700 + 6500/pixelScale, 0), 2)
 body3 = Body([200, 200, 200], Vec(-580, 0), 1736/pixelScale)
-body2.velocity.y = 10.976/300
-#10.976 escapes the moon orbit
+body2.velocity.y = 10.966/300
+#10.956 escapes the moon orbit
 
 active = True
 cycle = 0
@@ -33,29 +32,24 @@ while active:
    body3.Draw(screen)
    pointList.append(Vec.ToTuple(Body.PositionInPlane(screen, body2.position)))
 
-   distSQ = pow(body.position.x - body2.position.x, 2) + pow(body.position.y - body2.position.y, 2)
-   distSQ2 = pow(body3.position.x - body2.position.x, 2) + pow(body3.position.y - body2.position.y, 2)
-   auth_g = CalculateG(6.6743 * 10**-11, 5.9721 * 10**24, distSQ)
-   auth_g2 = CalculateG(6.6743 * 10**-11, 5.9721 * 10**24, distSQ2)
-
-   slope = math.atan2(body2.position.y - body.position.y,body2.position.x - body.position.x)
-   slope2 = math.atan2(body2.position.y - body3.position.y,body2.position.x - body3.position.x)
-   g = Vec((auth_g/(pixelScale * 1000)) * math.cos(slope), (auth_g/(pixelScale * 1000)) * math.sin(slope))
-   g2 = Vec((auth_g2/(pixelScale * 1000)) * math.cos(slope2), (auth_g2/(pixelScale * 1000)) * math.sin(slope2))
+   g ,distSQ   = PhyUpdate(body, body2, 5.9721 * 10**24)
+   g2,distSQ2  = PhyUpdate(body3, body2, 7.3476 * 10**22)
    body2.velocity = body2.velocity - (g+g2)
 
    pygame.draw.lines(screen, (255, 0, 0), False, pointList)
    pygame.draw.line(screen, (200, 0, 0), Vec.ToTuple(Body.PositionInPlane(screen, body2.position)),Vec.ToTuple(Body.PositionInPlane(screen, body.position)))
    
    text = my_font.render(f'{math.sqrt(distSQ) * pixelScale} Km', False, (255, 0, 0))
-   text2 = my_font.render(f'{body2.velocity.Mag() * pixelScale} km/s', False, (255, 0, 0))
-   text3 = my_font.render(f'{g.Mag() * (pixelScale * 1000)} m/s2', False, (255, 0, 0))
-   text4 = my_font.render(f'{cycle//3600} h', False, (255, 0, 0))
+   text2 = my_font.render(f'{math.sqrt(distSQ2) * pixelScale} Km', False, (255, 0, 0))
+   text3 = my_font.render(f'{body2.velocity.Mag() * pixelScale} km/s', False, (255, 0, 0))
+   text4 = my_font.render(f'{g.Mag() * (pixelScale * 1000)} m/s2', False, (255, 0, 0))
+   text5 = my_font.render(f'{cycle//3600} h', False, (255, 0, 0))
 
    screen.blit(text, (0,0))
    screen.blit(text2, (0,25))
    screen.blit(text3, (0,50))
    screen.blit(text4, (0,75))
+   screen.blit(text5, (0,100))
 
    cycle += 1
    pygame.display.update()
